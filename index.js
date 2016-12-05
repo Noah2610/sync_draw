7a4a4c03ef63b519677e98225b1fed7dfa3f8c59
Noah2610 <noah@rays-lab.com>
2016-12-05 10:05:44 +0100

    stuff changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var socket = require("socket.io");
 var chalk = require("chalk");
-// var path = require("path");
+var datetime = require("node-datetime");
 
 var app = express();
 var port = 3333;
@@ -14,17 +14,87 @@ console.log(chalk.green("server running on port " + port));
 
 
 var io = socket(server);
-
-io.sockets.on("connection", newConnection);
+var onlineArr = [];
+var storeLoginArr = [];
 
 function newConnection(socket) {
-	console.log(chalk.green("new connection: " + socket.id));
+	var ip = socket.handshake.address.substr(7);
+	var defName = "guest";
+	var name;
+		var check = false;
+	for (count = 0; count < storeLoginArr.length; count++) {
+		if (ip == storeLoginArr[count].ip) {
+			var name = storeLoginArr[count].name;
+			var check = true;
+			break;
+		}
+	}
+	if (!check) {
+		var name = defName;
+	}
+	onlineArr.push({id: socket.id, ip: ip, name: name});
+
+	console.log(chalk.green(chalk.underline(curDate("H:M:S")) + " - connected: " + chalk.bold(socket.id + " - " + ip + " - " + name)));
 
 	function mouseMsg(data) {
-		console.log(socket.id + " sending: " + data);
+		// console.log(socket.id + " sending: " + data);
 
-		socket.broadcast.emit("mouse", data);
+		socket.broadcast.emit("mouse", data);  // goes to every connection except this one
+
+		// io.sockets.emit("mouse", data);  // goes to every connection including this one
+	}
+
+	function addName(data) {
+		for (count = 0; count < onlineArr.length; count++) {
+			if (onlineArr[count].id == data.id) {
+				onlineArr[count].name = data.name;
+				io.sockets.emit("info", {online: onlineArr});
+
+				// add name to storeLoginArr
+				var check = false;
+				for (countStore = 0; countStore < storeLoginArr.length; countStore++) {  // check for existing entries
+					if (storeLoginArr[countStore].ip == onlineArr[count].ip) {
+						var check = true;
+						storeLoginArr[countStore].name = data.name;
+						break;
+					}
+				}
+				// add new index if no corresponding already exists
+				if (!check) {
+					storeLoginArr.push({ip: onlineArr[count].ip, name: data.name});
+				}
+
+				break;
+			}
+		}
 	}
 
 	socket.on("mouse", mouseMsg);
+	socket.on("info", addName);
+
+	io.sockets.emit("info", {online: onlineArr});
+
+	socket.on("disconnect", function () {  // disconnect
+
+		for (count = 0; count < onlineArr.length; count++) {
+			if (onlineArr[count].id == socket.id) {
+				console.log(chalk.red(chalk.underline(curDate("H:M:S")) + " - disconnected: " + chalk.bold(socket.id + " - " + ip + " - " + onlineArr[count].name)));
+				onlineArr.splice(count, 1);
+				var data = {
+					online: onlineArr,
+					index: count
+				};
+				break;
+			}
+		}
+		io.sockets.emit("info", data);
+
+	});
 }
+
+io.sockets.on("connection", newConnection);
+
+
+function curDate(frmt) {  // get date and/or time with format
+	return datetime.create().format(frmt);
+};
--- public/sketch.js
+++ public/sketch.js
@@ -1,27 +1,32 @@
 
 var socket;
 var port = 3333;
-var widthIncr = 5;
+var drawWHincr = 5;
 var cnv;
 var screenWidth = 0.9875;  // in percent
 var screenHeight = 0.98;  // in percent
+var drawWHdef = 30;
 
-var drawWidth = 30;
+var drawWH = drawWHdef;
 var drawColorArr = [
 	Math.floor((Math.random() * 255) + 1),
 	Math.floor((Math.random() * 255) + 1),
 	Math.floor((Math.random() * 255) + 1)
 ];
 
+var onlineArr = [];
+var socketID;
 
 function setup() {
 	cnv = createCanvas(Math.round(windowWidth * screenWidth), Math.round(windowHeight * screenHeight));
 	background(128,128,128);
 
 	socket = io.connect("http://84.113.223.39:" + port);
+	// socket = io.connect("http://noahlt.localtunnel.me");
 	socket.on("mouse", newDrawing);
+	socket.on("info", logInfo);
 
-	cnv.mouseWheel(changeWidth);
+	cnv.mouseWheel(changeWidth);  // add event listener
 
 }
 
@@ -31,33 +36,74 @@ function newDrawing(data) {
 	ellipse(data.x, data.y, data.width, data.width);
 }
 
+function logInfo(data) {
+	if (data === undefined) {  // if user executes function
+		console.log("current connection count: " + onlineArr.length);
+		for (var count = 0; count < onlineArr.length; count++) {
+			console.log(onlineArr[count].name);
+		}
+
+	} else if (typeof data == "object") {  // if client connects to socket
+		if (data.online.length > onlineArr.length) {  // new connection
+			if (socketID === undefined) {
+				socketID = data.online[data.online.length - 1].id;
+				console.log("Logged in as '" + data.online[data.online.length - 1].name + "'");
+			}
+			var str = "connected: ";
+			var targetIP = data.online[data.online.length - 1].ip;
+		} else if (data.online.length < onlineArr.length) {  // disconnect
+			var str = "disconnected: ";
+			var targetIP = onlineArr[data.index].ip;
+		} else {  // default
+			var str = false;
+		}
+		onlineArr = data.online;
+		if (str) {
+			console.log(str + targetIP);
+		}
+	}
+}
+
+function status(par) {
+	logInfo(par);
+}
+
+function login(name) {
+	if (name != "" && name !== undefined) {
+		var data = {
+			name: name,
+			id: socketID
+		};
+		socket.emit("info", data);
+		return "Logged in as '" + name + "'";
+	}
+}
+
 function keyPressed() {
-	if (keyCode == " ".charCodeAt(0)) {
+	if (keyCode == " ".charCodeAt(0)) {  // space - new random color
 		drawColorArr = [
 			Math.floor((Math.random() * 255) + 1),
 			Math.floor((Math.random() * 255) + 1),
 			Math.floor((Math.random() * 255) + 1)
 		];
+	} else if (keyCode == "X".charCodeAt(0)) {  // X - reset drawWH
+		drawWH = drawWHdef;
+	} else if (keyCode == "1".charCodeAt(0)) {  // 1 - set draw color white (tmp)
+		drawColorArr = [255,255,255];
+	} else if (keyCode == "2".charCodeAt(0)) {  // 2 - set draw color red (tmp)
+		drawColorArr = [255,0,0];
 	}
 }
 
 function changeWidth(event) {
 	if (event.deltaY < 0) {  // mouse wheel up
-		console.log("mouse wheel up");
-		drawWidth += widthIncr;
+		drawWH += drawWHincr;
 	} else if (event.deltaY > 0) {  // mouse wheel down
-		console.log("mouse wheel down");
-		drawWidth -= widthIncr;
+		drawWH -= drawWHincr;
 	}
 }
 
-function mouseWheel(changeWidth) {
-	console.log("mouse wheel used");
-};
-
-// function mouseWheel(event) {
-// 	console.log(event);
-// };
+function mouseWheel(changeWidth) {}
 
 function mouseDragged() {
 	noStroke();
@@ -66,12 +112,12 @@ function mouseDragged() {
 		x: mouseX,
 		y: mouseY,
 		color: drawColorArr,
-		width: drawWidth
+		width: drawWH
 	};
 	socket.emit("mouse", data);
 
 	fill(drawColorArr);
-	ellipse(mouseX, mouseY, drawWidth, drawWidth);
+	ellipse(mouseX, mouseY, drawWH, drawWH);
 }
 
 function draw() {
